551a613d2c483c50f24cfaf3391215d27802ff8f
rebo <rebo@fakeemail.org>
2020-02-09 03:02:34 +0000

    ev_handlers added

---
+++ src/ev_handlers_for_state_access.rs
@@ -0,0 +1,52 @@
+use comp_state::StateAccess;
+use seed::prelude::*;
+use web_sys;
+
+pub trait StateAccessEventHandlers<T>
+where
+    T: 'static,
+{
+    fn input_ev<F: Fn(&mut T, String) -> () + 'static + Clone, Ms: Default + 'static>(
+        &self,
+        event: Ev,
+        func: F,
+    ) -> seed::Listener<Ms>;
+
+    fn mouse_ev<F: Fn(&mut T, web_sys::MouseEvent) -> () + 'static + Clone, Ms: Default + 'static>(
+        &self,
+        event: Ev,
+        func: F,
+    ) -> seed::Listener<Ms>;
+}
+
+impl<T> StateAccessEventHandlers<T> for StateAccess<T>
+where
+    T: 'static,
+{
+    fn input_ev<F: Fn(&mut T, String) -> () + 'static + Clone, Ms: Default + 'static>(
+        &self,
+        event: Ev,
+        func: F,
+    ) -> seed::Listener<Ms> {
+        let accessor = *self;
+        input_ev(event, move |text| {
+            accessor.update(|val| func(val, text));
+            Ms::default()
+        })
+    }
+
+    fn mouse_ev<
+        F: Fn(&mut T, web_sys::MouseEvent) -> () + 'static + Clone,
+        Ms: Default + 'static,
+    >(
+        &self,
+        event: Ev,
+        func: F,
+    ) -> seed::Listener<Ms> {
+        let accessor = *self;
+        mouse_ev(event, move |m_ev| {
+            accessor.update(|val| func(val, m_ev));
+            Ms::default()
+        })
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,20 @@
 use comp_state::{topo, use_state, CloneState};
 use seed::{prelude::*, *};
 
+mod ev_handlers_for_state_access;
+use ev_handlers_for_state_access::StateAccessEventHandlers;
+
 #[derive(Default)]
 struct Model {}
 
 enum Msg {
     NoOp,
+}
+
+impl Default for Msg {
+    fn default() -> Msg {
+        Msg::NoOp
+    }
 }
 
 fn update(msg: Msg, _model: &mut Model, _: &mut impl Orders<Msg>) {
@@ -28,6 +37,8 @@ fn root_view() -> Node<Msg> {
         my_button(),
         my_button(),
         my_button(),
+        my_ev_button(),
+        my_ev_input(),
     ]
 }
 
@@ -41,6 +52,26 @@ fn my_button() -> Node<Msg> {
             Msg::NoOp
         })
     ]]
+}
+
+#[topo::nested]
+fn my_ev_button() -> Node<Msg> {
+    let count_access = use_state(|| 0);
+    div![button![
+        format!("Clicked {} times", count_access.get()),
+        count_access.mouse_ev(Ev::Click, |count, _| *count += 1),
+    ]]
+}
+fn my_ev_input() -> Node<Msg> {
+    let input_access = use_state(|| "".to_string());
+
+    div![
+        input![
+            attrs![ At::Value => input_access.get()],
+            input_access.input_ev(Ev::Input, |input, text| *input = text),
+        ],
+        format!("Text inputted: {}", input_access.get())
+    ]
 }
 
 #[wasm_bindgen(start)]
